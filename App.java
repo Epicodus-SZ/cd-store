331f0dbd60c133c88952723ee0859d829b23ce5d
at and sz <at  anjatimofeyeva@gmail.com sz  github@zaske.com>
2017-06-27 09:40:13 -0700

    create CD object and test

---
+++ App.java
@@ -0,0 +1,7 @@
+public class App {
+  public static void main(String[] args){
+    //String intName, String intAlbum,int intReleaseYear, int intPrice
+    Cd myCD = new Cd("Pink Floyd","Darkside of the Moon",1973,33);
+    System.out.println(myCD.album);
+  }
+}
