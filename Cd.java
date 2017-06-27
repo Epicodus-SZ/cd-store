5fdeed7b88b808874de9837ee48c96cbf09ace68
at and sz <at  anjatimofeyeva@gmail.com sz  github@zaske.com>
2017-06-27 09:35:16 -0700

    Initial CD object

---
+++ Cd.java
@@ -0,0 +1,13 @@
+class Cd {
+  String name;
+  String album;
+  int releaseYear;
+  int price;
+
+  public Cd(String intName, String intAlbum,int intReleaseYear, int intPrice) {
+    name = intName;
+    album = intAlbum;
+    releaseYear = intReleaseYear;
+    price = intPrice;
+  }
+}
